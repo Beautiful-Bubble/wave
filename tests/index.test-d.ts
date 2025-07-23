485b3bd92064dfd8a517afa43bc9f8892750c9b5
Li Zhineng <im@zhineng.li>
2025-07-23 21:43:56 +0800

    apply fixes by Prettier

--- tests/index.test-d.ts
+++ tests/index.test-d.ts
@@ -11,17 +11,14 @@ interface User {
     response.data()
   )
 }
-
 ;async () => {
   const response = await Client.new().get<User>('http://example.com')
   expectType<User>(response.data())
 }
-
 ;async () => {
   const response = await Client.new().get<string>('http://example.com/string')
   expectType<string>(response.data())
 }
-
 ;async () => {
   const response = await Client.new().upload({
     url: 'http://example.com',
